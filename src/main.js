28d256a38d4eafe5a94b5240dd7426d48447d266
AnisDjerrab <anisdjerrab@gmail.com>
2026-05-05 18:44:30 +0100

    important progress on both frontend & backend

--- src/main.js
+++ src/main.js
@@ -28,7 +28,23 @@ document.addEventListener("click", (e) => {
   }
 });
 
-document.querySelector(".path").textContent = "hello world"
+// get the file grid element
+const file_grid = document.querySelector(".file_icons_grid");
+
 // now, communicate with rust to get the 'home' path
-invoke('get_home_directory').then((home_dir) => document.querySelector(".path").textContent = home_dir);
+invoke('get_home_directory').then((home_dir) => document.querySelector(".path_area").value = home_dir + "/");
 // try to communicate with rust to get the files in home
+invoke('ls_dir', {path : "/home/anis/Documents/OpenGL-apps/", sortMethod : "A-Z", sortMethodDfs : "dfs"}).then((output) => {
+  for (let i = 0; i < output[0].length / 2; i++) {
+    const file_icon = document.createElement("div");
+    file_icon.className = "file_icon";
+    const img = document.createElement("img");
+    img.src = output[0][i + output[0].length / 2];
+    const icon_content = document.createElement("div");
+    icon_content.textContent = output[0][i];
+    file_icon.appendChild(img);
+    file_icon.appendChild(icon_content);
+    file_grid.appendChild(file_icon);
+  }
+});
+
